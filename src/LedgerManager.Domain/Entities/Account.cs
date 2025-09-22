e2d18ef4d6964cb40b4925774932abb2f7cac5a0
Valerons <Valeriy.Sardin@yandex.ru>
2025-09-22 13:45:48 +0500

    feat(domain): add Account and Resident entities

---
+++ src/LedgerManager.Domain/Entities/Account.cs
@@ -0,0 +1,13 @@
+﻿namespace LedgerManager.Domain.Entities;
+
+public class Account
+{
+    public Guid Id { get; set; }
+    public required string AccountNumber { get; set; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime EndDate { get; set; }
+    public required string Address { get; set; }
+    public required int Area { get; set; }
+    
+    public List<Resident> Residents { get; set; } = new();
+}
---
+++ src/LedgerManager.Domain/Entities/Resident.cs
@@ -0,0 +1,12 @@
+﻿namespace LedgerManager.Domain.Entities;
+
+public class Resident
+{
+    public Guid Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public DateTime? BirthDate { get; set; } = null!;
+    public required Guid AccountId { get; set; }
+    
+    public Account? Account { get; set; } = null!;
+}
