f6f9b3c34c3988a28f4bdd1625792c56c370a04d
Valerons <Valeriy.Sardin@yandex.ru>
2025-09-24 16:57:16 +0500

    feat(api): add custom middleware

--- src/LedgerManager.Application/obj/Debug/net9.0/LedgerManager.Application.AssemblyInfo.cs
+++ src/LedgerManager.Application/obj/Debug/net9.0/LedgerManager.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LedgerManager.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa1fb6b7a496a42c8ea5b982bc0aa00fc3bc578")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d1180b3f557d71d49722c2ba335adbce42aa5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LedgerManager.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LedgerManager.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
